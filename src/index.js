286614e81ffd75fa3b923b48b4d4c2d165e18d2d
Luiz <luizribeirocorreia09@gmail.com>
2026-04-14 18:56:43 -0300

    Add tab nav menu

--- src/index.js
+++ src/index.js
@@ -6,5 +6,23 @@ import {about} from "./about.js";
 // get div by id
 export const contentDiv = document.querySelector("#content");
 
+const homeBtn = document.querySelector(".home-btn");
+const menuBtn = document.querySelector(".menu-btn");
+const aboutBtn = document.querySelector(".about-btn");
+
+homeBtn.addEventListener("click", () => {
+    resetContent();
+    homepage();
+});
+
+menuBtn.addEventListener("click", () => {
+    resetContent();
+    menu();
+
+});
+
+aboutBtn.addEventListener("click", () => {
+    resetContent();
+    about();
+});
 console.log("restaurant has opened");
-about();
